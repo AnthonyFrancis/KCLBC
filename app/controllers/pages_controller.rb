bd7d57ec32ba0b5ad79a38e60a6f9ec45d87ef5d
Anthony Francis <anthony@getpeekapp.com>
2014-06-15 13:49:29 +0100

    added pages

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,23 @@
+class PagesController < ApplicationController
+  def home
+  end
+
+  def about
+  end
+
+  def speakers
+  end
+
+  def press
+  end
+
+  def chairman
+  end
+
+  def contact
+  end
+
+  def rsvp
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 KCLBC::Application.routes.draw do
+  root "pages#home"
+  get "about" => "pages#about"
+  get "speakers" => "pages#speakers"
+  get "press" => "pages#chairman"
+  get "contact" => "pages#contact"
+  get "rsvp" => "pages#rsvp"
+
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
