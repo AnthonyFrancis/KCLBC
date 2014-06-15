d4ec03a64cc90722161a9867d771108ff4bbc8dd
Anthony Francis <anthony@getpeekapp.com>
2014-06-15 13:34:29 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+KCLBC::Application.config.secret_key_base = '2227036cc45ada76441dc978b74ac80dfb2a85e1c6e68b9d65e0b8c50f51ef59deac7b1af5b2f9dab7c8a13e39ef96f9c66d8b8e428c121439966f96e1b1746b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+KCLBC::Application.config.session_store :cookie_store, key: '_KCLBC_session'
